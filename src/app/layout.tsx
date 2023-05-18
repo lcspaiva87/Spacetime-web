72223bf994a5ae5ffa48f4d25430643578272648
lcspaiva <lcspaiva87@gmail.com>
2023-05-18 13:11:45 -0300

    fix: create page home

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './globals.css'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamJuree,
+} from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamJuree = BaiJamJuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-baiJamJuree',
+})
+export const metadata = {
+  title: 'NLW Spacetime',
+  description:
+    'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e Typescript.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baiJamJuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
